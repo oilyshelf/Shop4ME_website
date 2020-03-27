80578bf1c608174470df47b7c750f83f36c8310a
rosti <rostislav.iskandirov@student.uni-siegen.de>
2020-03-27 19:46:40 +0100

    login component done and route created

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import {UserRoute} from "./Routes/userRoute";
 import Home from "./Components/Allgemein/Home";
 import {connect} from "./Connection/Websocket";
-
+import 'antd/dist/antd.css';
+import Login from "./Components/Bringer/Login";
 
 
 function App() {
 
     useEffect(()=>{
        //connect();
-    });
+    },[]);
 
   return (
       <Router>
@@ -18,7 +19,7 @@ function App() {
 
           <Switch>
             <Route path = "/" exact component={Home} />
-
+            <Route path = "/login" exact component={Login}/>
           </Switch>
 
         </div>
---
+++ src/Components/Bringer/Login.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+import "./Login.css"
+import Home from "../Allgemein/Home";
+import {Checkbox} from "antd";
+
+function Login() {
+
+    const [loginInfos,setLoginin] = useState({email:"", password: ""});
+
+    const array = [{name:"mehl", anzahl:2}, {name:"zucker", anzahl: 3}];
+
+    const emailChanger =(event)=>{
+        setLoginin({
+            email: event.target.value,
+            password: loginInfos.password
+        });
+    };
+    const passwordChanger =(event)=>{
+        setLoginin({
+            email: loginInfos.email,
+            password: event.target.value
+        });
+    };
+
+    const sendLoginDaten=()=>{
+      console.log(loginInfos.email + " "+ loginInfos.password);
+    };
+
+    const ar = array.map(a=>
+        <li key={a.name}>{a.name} anzahl = {a.anzahl}</li>
+    );
+    const checkboxchanger =(e)=>{
+        console.log(`checked = ${e.target.checked}`);
+    }
+
+
+    return(
+        <div className="Login">
+            <h1>Login</h1>
+            <div>
+                <input  value={loginInfos.email} placeholder="email" onChange={emailChanger.bind(this)}/>
+                <input type="password" placeholder="password" value={loginInfos.password} onChange={passwordChanger.bind(this)}/>
+                <button  onClick={sendLoginDaten}>Log in</button>
+                <Checkbox onChange={checkboxchanger}>eingeloggt bleiben</Checkbox>
+               <ul>{ar}</ul>
+            </div>
+        </div>
+    );
+
+
+}
+
+export default Login;
